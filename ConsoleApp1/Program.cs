e9f350612bf23038bfee0e024194232d5c1771bc
aptem1223 <sanstop357@gmail.com>
2025-06-05 11:01:58 +0300

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    enum Month
+    {
+        Январь = 1,
+        Февраль,
+        Март,
+        Апрель,
+        Май,
+        Июнь,
+        Июль,
+        Август,
+        Сентябрь,
+        Октябрь,
+        Ноябрь,
+        Декабрь
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Месяцы года:");
+            foreach (Month month in Enum.GetValues(typeof(Month)))
+            {
+                Console.WriteLine($"{(int)month}. {month}");
+            }
+            Month currentMonth = Month.Июнь;
+            Console.WriteLine($"\nТекущий месяц: {currentMonth} ({(int)currentMonth})");
+            Console.Read();
+        }
+    }
+}
