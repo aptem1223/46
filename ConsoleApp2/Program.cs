3351138167efc697b77684fec08d036bb2ac1d1b
aptem1223 <sanstop357@gmail.com>
2025-06-05 11:02:20 +0300

    Add files via upload

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+        enum WeekDay
+        {
+            Monday = 1,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Console.WriteLine("Дни недели:");
+                foreach (WeekDay day in Enum.GetValues(typeof(WeekDay)))
+                {
+                    Console.WriteLine($"{(int)day}. {day}");
+                }
+                WeekDay today = WeekDay.Wednesday;
+                Console.WriteLine($"\nСегодня: {today} ({(int)today})");
+                Console.Read();
+            }
+        }
+}
